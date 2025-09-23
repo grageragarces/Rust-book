b4a3dbf033d3fafc2147347dc1d97b89e425b94b
Maria Gragera-Garces <mariagg-quantinuum>
2025-09-23 17:04:22 +0100

    Day 1: Chap 1 & half of 2

---
+++ target/debug/build/libc-2337c9d59abc38a6/build_script_build-2337c9d59abc38a6.d
@@ -0,0 +1,5 @@
+/Users/maria.gragera-garces/Documents/Rust learning/guessing_game/target/debug/build/libc-2337c9d59abc38a6/build_script_build-2337c9d59abc38a6.d: /Users/maria.gragera-garces/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.176/build.rs
+
+/Users/maria.gragera-garces/Documents/Rust learning/guessing_game/target/debug/build/libc-2337c9d59abc38a6/build_script_build-2337c9d59abc38a6: /Users/maria.gragera-garces/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.176/build.rs
+
+/Users/maria.gragera-garces/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.176/build.rs:
